fa5cf4ab305bbb35ed357c33cff5b4e9c0ee13e7
Vinicius Faria <viniciusfaria.vmf@gmail.com>
2023-02-15 22:42:55 -0300

    First commit

---
+++ template/api.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'api_key=4f4d83ea46e08a68241e86ac38f8d82a';
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+export const language = 'language=pt-BR';   
---
+++ template/main.js
@@ -0,0 +1,7 @@
+ import {
+  API_KEY, 
+  BASE_URL,
+  IMG_URL,
+  language,
+ } from './api.js'
+
